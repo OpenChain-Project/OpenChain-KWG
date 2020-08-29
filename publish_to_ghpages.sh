cd36a39b11ea2423925e0f3cfd8623c0010370ef
Haksung Jang <haksung@sk.com>
2020-08-29 15:18:28 +0900

    create a script for deploy

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -22,7 +22,7 @@ echo "Generating site"
 env HUGO_ENV="production" hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages"
 
 echo "Pushing to github"
 git push --all
